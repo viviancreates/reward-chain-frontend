0ff497632a7455ce4d080868c1ccd0454019fc34
Vivian Davila <viviandavilacodes@gmail.com>
2025-08-28 10:27:29 -0700

    Clean up some files

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import './styles/base.css';
 import NavBar from './components/NavBar';
 import Register from './views/Register';
 import Login from './views/Login';
-import Profile from './views/Profile';
 import RequireAuth from './components/RequireAuth';
-import Transactions from './views/Transactions';
 import Analytics from './views/Analytics';
-import Rules from './views/Rules';
-import Categories from './views/Categories';
 import Rewards from './views/Rewards';
 import Landing from "./views/Landing";
-
+import Profile from "./views/ProfileDashboard"
 import Spend from './views/Spend';
 import Catalog from './views/Catalog';
-import ProfileDashboard from './views/ProfileDashboard';
+
 
 export default function App() {
   return (
@@ -26,16 +22,11 @@ export default function App() {
         <Route path="/login" element={<Login />} />
 
         {/* protected pages */}
-        <Route path="/transactions" element={<RequireAuth><Transactions /></RequireAuth>} />
         <Route path="/analytics" element={<RequireAuth><Analytics /></RequireAuth>} />
-        <Route path="/rules" element={<RequireAuth><Rules /></RequireAuth>} />
-        <Route path="/categories" element={<RequireAuth><Categories /></RequireAuth>} />
         <Route path="/rewards" element={<RequireAuth><Rewards /></RequireAuth>} />
-        <Route path="/profile" element={<RequireAuth><ProfileDashboard /></RequireAuth>} />
-        <Route path="/spend" element={<RequireAuth><Spend /></RequireAuth>} />
+        <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>} />
         <Route path="/catalog" element={<RequireAuth><Catalog /></RequireAuth>} />
-        <Route path="/rewards" element={<RequireAuth><Rewards /></RequireAuth>
-        } />
+        <Route path="/spend" element={<RequireAuth><Spend /></RequireAuth>} />
       </Routes>
     </BrowserRouter>
   );
